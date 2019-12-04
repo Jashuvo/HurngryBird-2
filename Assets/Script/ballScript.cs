c20f95fa3b7c30f51713265ae2e44c1a08f5dd69
Jubayed Ahmed <40147968+Jashuvo@users.noreply.github.com>
2019-12-04 15:55:01 +0600

    init

--- Assets/Script/ballScript.cs
+++ Assets/Script/ballScript.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class ballScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool isPulled = false;
+    public Rigidbody2D ballRigid;
+    public float releaseTime = 0.15f;
+
     void Start()
     {
-        
+        ballRigid = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isPulled)
+        {
+            ballRigid.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isPulled = true;
+        ballRigid.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPulled = false;
+        ballRigid.isKinematic = false;
+        StartCoroutine(Release()) ;
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
     }
 }
---
+++ Assets/Script/enemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    public float enemyHealth = 4f;
+    
+    void OnCollisionEnter2D(Collision2D enemyCollsion)
+    {
+
+        if (enemyCollsion.relativeVelocity.magnitude > enemyHealth)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
+;
